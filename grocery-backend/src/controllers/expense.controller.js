1a9bfa0d2b0ea5bb6c4faaef73194dc586697769
Sarthak-12p <sarthakphadtare5000@gmail.com>
2026-08-19 08:44:21 +0530

    mongoose was not defind in expense controller

--- grocery-backend/src/controllers/expense.controller.js
+++ grocery-backend/src/controllers/expense.controller.js
@@ -2,6 +2,7 @@ import asyncHandler from "../utils/asyncHandler.js";
 import ApiError from "../utils/ApiError.js";
 import ApiResponse from "../utils/ApiResponse.js";
 import Expense from "../models/expense.model.js";
+import mongoose from "mongoose";
 
 export const createExpense = asyncHandler(async (req, res) => {
   const { title, amount, category, date , description  } = req.body;
